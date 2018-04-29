d608c1e514d7f7b098cf2edde0ffd15bcc8bdd5e
itos8 <vitoscornavacche@live.it>
2018-04-29 12:17:27 +0200

    bug Spring Configuration solved

--- src/main/kotlin/com/polito/kotlin/RestTurism/RestTurismApplication.kt
+++ src/main/kotlin/com/polito/kotlin/RestTurism/RestTurismApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RestTurismApplication
+open class RestTurismApplication
 
 fun main(args: Array<String>) {
     mongoCodec()
